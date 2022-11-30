cad849c4fa0dce7338af5ebb84a1ca9bc67df46b
LehaGer <leha.ger97@mail.ru>
2022-11-30 20:56:29 +0300

    feat: implemented file creation

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,42 @@
+import { writeFile, access, constants  } from 'node:fs/promises';
+import { Buffer } from 'node:buffer';
+
+const FILE_CONTENT_TXT = 'I am fresh and young';
+const FILE_EXCEPTION_ERROR_MSG = 'FS operation failed';
+const FILE_PATH = new URL('./files/fresh.txt', import.meta.url);
+
+const checkIfFileExists = async (path) => {
+
+    await access(path, constants.F_OK);
+
+    throw new Error(FILE_EXCEPTION_ERROR_MSG);
+
+}
+
 const create = async () => {
-    // Write your code here 
+
+    try {
+
+        await checkIfFileExists(FILE_PATH);
+
+    } catch (error) {
+
+        if(error.message !== FILE_EXCEPTION_ERROR_MSG) {
+
+            const data = new Uint8Array(Buffer.from(FILE_CONTENT_TXT));
+            const promise = writeFile(FILE_PATH, data);
+
+            await promise;
+
+        } else {
+
+            throw error;
+
+        }
+
+    }
+
 };
 
 await create();
+
