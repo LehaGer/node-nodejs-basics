754d3e379612cef5133465d9b6fd44d5af6368a7
LehaGer <leha.ger97@mail.ru>
2022-12-02 15:16:14 +0300

    feat: implemented reading

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,46 @@
+import { access, constants, readFile } from 'node:fs/promises';
+
+const FILE_EXCEPTION_ERROR_MSG = 'FS operation failed';
+const TARGET_FILE_URL = new URL('./files/fileToRead.txt', import.meta.url);
+
+const doesPathExists = async (path) => {
+
+    try {
+
+        await access(path, constants.F_OK);
+
+        return true;
+
+    } catch (e) {
+
+        return false;
+    }
+
+}
+const checkIfTargetFileExists = async () => {
+
+    if(!await doesPathExists(TARGET_FILE_URL)) {
+
+        throw new Error(FILE_EXCEPTION_ERROR_MSG);
+
+    }
+
+}
+
 const read = async () => {
-    // Write your code here 
+
+    try {
+
+        await checkIfTargetFileExists();
+
+        console.log(await readFile(TARGET_FILE_URL, { encoding: 'utf8' }));
+
+    } catch (error) {
+
+        throw error;
+
+    }
+
 };
 
 await read();
