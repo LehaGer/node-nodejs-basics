5e74f1aee129f9f93562f54584081eb645e36b3e
LehaGer <leha.ger97@mail.ru>
2022-12-02 16:25:23 +0300

    fix: fixed logging format of environment processing

--- src/cli/env.js
+++ src/cli/env.js
@@ -4,8 +4,9 @@ const parseEnv = () => {
 
     Object.entries(process.env)
         .filter(envVar => envVar[0].startsWith('RSS_'))
-        .forEach(rssVar => {
-            resultingStr = resultingStr.concat(`${rssVar[0]}=${rssVar[1]}; `)
+        .forEach((rssVar, index, array) => {
+            resultingStr = resultingStr.concat(`${rssVar[0]}=${rssVar[1]}`);
+            if(index < array.length - 1 ) resultingStr = resultingStr.concat(`; `);
         });
 
     console.log(resultingStr);
