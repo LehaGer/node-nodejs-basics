2034ec9b662ac5524d5a015128b35dd727ac0d28
LehaGer <leha.ger97@mail.ru>
2022-12-02 15:02:13 +0300

    feat: implemented deleting

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,47 @@
+import { access, constants, rm } from 'node:fs/promises';
+
+const FILE_EXCEPTION_ERROR_MSG = 'FS operation failed';
+const FILE_TO_REMOVE = new URL('./files/fileToRemove.txt', import.meta.url);
+
+const doesPathExists = async (path) => {
+
+    try {
+
+        await access(path, constants.F_OK);
+
+        return true;
+
+    } catch (e) {
+
+        return false;
+    }
+
+}
+const checkIfRemovingFileExists = async () => {
+
+    if(!await doesPathExists(FILE_TO_REMOVE)) {
+
+        throw new Error(FILE_EXCEPTION_ERROR_MSG);
+
+    }
+
+}
+
 const remove = async () => {
-    // Write your code here 
+
+    try {
+
+        await checkIfRemovingFileExists();
+
+        await rm(FILE_TO_REMOVE);
+
+
+    } catch (error) {
+
+        throw error;
+
+    }
+
 };
 
 await remove();
