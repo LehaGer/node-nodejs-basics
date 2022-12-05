bff618e84eab4cfdcbf85a6c44cb26b58c30b850
LehaGer <leha.ger97@mail.ru>
2022-12-05 03:56:09 +0300

    feat: implemented hash creating from file

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,16 @@
+import { createReadStream } from 'node:fs';
+import { stdout } from 'node:process';
+const { createHash } = await import('node:crypto');
+
+const TARGET_FILE_URL = new URL('./files/fileToCalculateHashFor.txt', import.meta.url);
+
 const calculateHash = async () => {
-    // Write your code here 
+
+    const hash = createHash('sha256');
+
+    const input = createReadStream(TARGET_FILE_URL);
+    input.pipe(hash).setEncoding('hex').pipe(stdout);
+
 };
 
 await calculateHash();
