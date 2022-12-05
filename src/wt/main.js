47dd2d22409db2880b7a42a1c99bd9b63125bafe
LehaGer <leha.ger97@mail.ru>
2022-12-05 17:40:13 +0300

    feat: implemented wt functionality

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,50 @@
+import { Worker } from 'node:worker_threads';
+import { cpus } from 'node:os';
+
+let INITIAL_INPUT_VALUE = 10;
+const WORKER_FILE_URL = new URL('./worker.js', import.meta.url);
+
 const performCalculations = async () => {
-    // Write your code here
+
+    const cpuCores = cpus();
+    const resultingArray = [];
+
+    cpuCores.forEach((_, index, array) => {
+
+        let result,
+            status = 'resolved';
+
+        const wt = new Worker(WORKER_FILE_URL, {
+            workerData: INITIAL_INPUT_VALUE++
+        });
+
+        wt.on('message', msg => {
+
+            if(msg?.type === 'computedValue') {
+                result = msg?.payload;
+            }
+
+        });
+        wt.on('error', () => {
+
+            status = 'error';
+
+        });
+        wt.on('exit', () => {
+
+            resultingArray[index] = {
+                status: status,
+                data: result,
+            };
+
+            if(resultingArray.filter(el => !!el).length === array.length) {
+                console.log(resultingArray);
+            }
+
+        });
+
+    })
+
 };
 
 await performCalculations();
