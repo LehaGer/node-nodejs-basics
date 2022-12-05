0d286b96fa43ac5dbc7bf86242e5a9cde94736d3
LehaGer <leha.ger97@mail.ru>
2022-12-05 13:58:42 +0300

    feat: implemented stream file reading

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,14 @@
+import { createReadStream } from 'node:fs';
+import { stdout } from 'node:process';
+
+const TARGET_FILE_URL = new URL('./files/fileToRead.txt', import.meta.url);
+
 const read = async () => {
-    // Write your code here 
+
+    const file = createReadStream(TARGET_FILE_URL);
+
+    file.pipe(stdout);
+
 };
 
 await read();
